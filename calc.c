50b18c75e47c1fc3a5c6af93078e7810cbacecd6
Taranathpoudel <44458069+Taranathpoudel@users.noreply.github.com>
2021-03-22 22:16:06 +0545

    Add files via upload

---
+++ calc.c
@@ -0,0 +1,19 @@
+
+int main()
+{
+int a;
+int b;
+int c;
+printf("Enter A num=");
+scanf("%d",&a);
+printf("\n");
+printf("Enter a num=");
+scanf("%d",&b);
+c=a+b;
+printf("Sum=%d",c);
+getch();
+return 0;
+
+
+
+}
